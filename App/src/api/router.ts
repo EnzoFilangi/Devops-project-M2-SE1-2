9111609795e782c51dded297c1287d152e71061e
EnzoFilangi <enzo.filangi@efrei.net>
2023-01-22 14:44:26 +0100

    Exposed routes to API

--- App/src/api/router.ts
+++ App/src/api/router.ts
@@ -1,8 +1,20 @@
 import { Router } from 'express';
 import {helloWorld} from "./routes/hello-world.route";
+import {getAllProducts} from "./routes/get-all-products.route";
+import {ProductRepository} from "./persistence/product-repository";
+import {getProduct} from "./routes/get-product.route";
+import {addProduct} from "./routes/add-product.route";
+import {deleteProduct} from "./routes/delete-product.route";
+import {updateProduct} from "./routes/update-product.route";
 
+const repository = new ProductRepository()
 const router = Router();
 
 router.get("/hello", helloWorld);
+router.get("/products", (req, res) => getAllProducts(req, res, repository))
+router.post("/products", (req, res) => addProduct(req, res, repository))
+router.get("/products/:productId", (req, res) => getProduct(req, res, repository))
+router.delete("/products/:productId", (req, res) => deleteProduct(req, res, repository))
+router.put("/products/:productId", (req, res) => updateProduct(req, res, repository))
 
 export default router;
